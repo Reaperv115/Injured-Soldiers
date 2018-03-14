5b479b4b27122ab79904b6ee0d910f7db74a6d9e
rjs5785 <rjs5785@hotmail>
2018-03-13 22:04:47 -0400

    slightly adjusted the zoom out

--- Hardware lab 6/Hardware lab 6/main.cpp
+++ Hardware lab 6/Hardware lab 6/main.cpp
@@ -844,9 +844,9 @@ void DEMO_APP::Render()
 		DEMO_APP::degVal += XMConvertToRadians(0.5f);
 		m.perspectiveMat = XMMatrixPerspectiveFovLH(DEMO_APP::degVal, 1, .1, 1000.0f);
 
-		if (DEMO_APP::degVal >= XMConvertToRadians(130.0f))
+		if (DEMO_APP::degVal >= XMConvertToRadians(115.0f))
 		{
-			DEMO_APP::degVal = XMConvertToRadians(130.0f);
+			DEMO_APP::degVal = XMConvertToRadians(115.0f);
 			m.perspectiveMat = XMMatrixPerspectiveFovLH(DEMO_APP::degVal, 1, .1, 1000.0f);
 		}
 	}
